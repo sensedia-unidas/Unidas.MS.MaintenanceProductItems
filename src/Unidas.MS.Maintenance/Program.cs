fbdf773e94fa1613517cb61e0621f276e91c75d0
Cristiano Joaquim dos Santos <ag.cristiano@unidas.com.br>
2022-11-11 12:57:49 -0300

    Inclusao do Projeto Consumer em Workservice e alteracao do nome do Projeto

---
+++ src/Unidas.MS.Maintenance/Program.cs
@@ -0,0 +1,10 @@
+using Unidas.MS.Maintenance;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
